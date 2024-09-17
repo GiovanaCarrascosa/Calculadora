f940c56a20b67d80de0f0c113ea7313a2a32f674
GiovanaCarrascosa <giovanaa.carrascosa194@gmail.com>
2024-09-17 15:00:05 -0300

    fiz o design da tela da calculadora

--- TelaCalculadora.Designer.cs
+++ TelaCalculadora.Designer.cs
@@ -29,68 +29,335 @@
         private void InitializeComponent()
         {
             System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(TelaCalculadora));
-            pictureBox1 = new PictureBox();
             pictureBox2 = new PictureBox();
-            ((System.ComponentModel.ISupportInitialize)pictureBox1).BeginInit();
+            button1 = new Button();
+            textBox1 = new TextBox();
+            button2 = new Button();
+            button3 = new Button();
+            button4 = new Button();
+            button5 = new Button();
+            button6 = new Button();
+            button7 = new Button();
+            button8 = new Button();
+            button9 = new Button();
+            button10 = new Button();
+            button11 = new Button();
+            button12 = new Button();
+            button13 = new Button();
+            button14 = new Button();
+            button15 = new Button();
+            button16 = new Button();
+            pictureBox1 = new PictureBox();
+            pictureBox3 = new PictureBox();
+            pictureBox4 = new PictureBox();
+            pictureBox5 = new PictureBox();
             ((System.ComponentModel.ISupportInitialize)pictureBox2).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)pictureBox1).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)pictureBox3).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)pictureBox4).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)pictureBox5).BeginInit();
             SuspendLayout();
-            // 
-            // pictureBox1
-            // 
-            pictureBox1.Image = (Image)resources.GetObject("pictureBox1.Image");
-
-            pictureBox1.Location = new Point(-25, 228);
-            pictureBox1.Name = "pictureBox1";
-            pictureBox1.Size = new Size(285, 247);
-            pictureBox1.TabIndex = 2;
-
-            pictureBox1.Location = new Point(-39, 237);
-            pictureBox1.Name = "pictureBox1";
-            pictureBox1.Size = new Size(285, 247);
-            pictureBox1.TabIndex = 0;
-
-            pictureBox1.TabStop = false;
             // 
             // pictureBox2
             // 
             pictureBox2.BackgroundImage = (Image)resources.GetObject("pictureBox2.BackgroundImage");
             pictureBox2.BackgroundImageLayout = ImageLayout.Zoom;
-
-            pictureBox2.Location = new Point(327, -6);
+            pictureBox2.Location = new Point(0, 361);
             pictureBox2.Name = "pictureBox2";
             pictureBox2.Size = new Size(105, 83);
             pictureBox2.TabIndex = 3;
-
-            pictureBox2.Location = new Point(331, 0);
-            pictureBox2.Name = "pictureBox2";
-            pictureBox2.Size = new Size(105, 83);
-            pictureBox2.TabIndex = 1;
-
             pictureBox2.TabStop = false;
+            // 
+            // button1
+            // 
+            button1.BackColor = Color.GhostWhite;
+            button1.Font = new Font("Swis721 BlkCn BT", 14.25F, FontStyle.Regular, GraphicsUnit.Point, 0);
+            button1.Location = new Point(111, 79);
+            button1.Name = "button1";
+            button1.Size = new Size(60, 54);
+            button1.TabIndex = 4;
+            button1.Text = "+";
+            button1.UseVisualStyleBackColor = false;
+            // 
+            // textBox1
+            // 
+            textBox1.Location = new Point(12, 12);
+            textBox1.Multiline = true;
+            textBox1.Name = "textBox1";
+            textBox1.Size = new Size(403, 45);
+            textBox1.TabIndex = 11;
+            textBox1.TextAlign = HorizontalAlignment.Center;
+            // 
+            // button2
+            // 
+            button2.BackColor = Color.GhostWhite;
+            button2.Font = new Font("Swis721 BlkCn BT", 14.25F, FontStyle.Regular, GraphicsUnit.Point, 0);
+            button2.Location = new Point(190, 79);
+            button2.Name = "button2";
+            button2.Size = new Size(60, 54);
+            button2.TabIndex = 12;
+            button2.Text = "-";
+            button2.UseVisualStyleBackColor = false;
+            // 
+            // button3
+            // 
+            button3.BackColor = Color.GhostWhite;
+            button3.Font = new Font("Swis721 BlkCn BT", 14.25F, FontStyle.Regular, GraphicsUnit.Point, 0);
+            button3.Location = new Point(270, 79);
+            button3.Name = "button3";
+            button3.Size = new Size(60, 54);
+            button3.TabIndex = 13;
+            button3.Text = "*";
+            button3.UseVisualStyleBackColor = false;
+            // 
+            // button4
+            // 
+            button4.BackColor = Color.GhostWhite;
+            button4.Font = new Font("Swis721 BlkCn BT", 14.25F, FontStyle.Regular, GraphicsUnit.Point, 0);
+            button4.Location = new Point(345, 79);
+            button4.Name = "button4";
+            button4.Size = new Size(60, 54);
+            button4.TabIndex = 14;
+            button4.Text = "/";
+            button4.UseVisualStyleBackColor = false;
+            // 
+            // button5
+            // 
+            button5.BackColor = Color.GhostWhite;
+            button5.Font = new Font("Swis721 BlkCn BT", 14.25F, FontStyle.Regular, GraphicsUnit.Point, 0);
+            button5.Location = new Point(111, 148);
+            button5.Name = "button5";
+            button5.Size = new Size(60, 54);
+            button5.TabIndex = 15;
+            button5.Text = "1";
+            button5.UseVisualStyleBackColor = false;
+            // 
+            // button6
+            // 
+            button6.BackColor = Color.GhostWhite;
+            button6.Font = new Font("Swis721 BlkCn BT", 14.25F, FontStyle.Regular, GraphicsUnit.Point, 0);
+            button6.Location = new Point(190, 148);
+            button6.Name = "button6";
+            button6.Size = new Size(60, 54);
+            button6.TabIndex = 16;
+            button6.Text = "2";
+            button6.UseVisualStyleBackColor = false;
+            // 
+            // button7
+            // 
+            button7.BackColor = Color.GhostWhite;
+            button7.Font = new Font("Swis721 BlkCn BT", 14.25F, FontStyle.Regular, GraphicsUnit.Point, 0);
+            button7.Location = new Point(270, 148);
+            button7.Name = "button7";
+            button7.Size = new Size(60, 54);
+            button7.TabIndex = 17;
+            button7.Text = "3";
+            button7.UseVisualStyleBackColor = false;
+            // 
+            // button8
+            // 
+            button8.BackColor = Color.GhostWhite;
+            button8.Font = new Font("Swis721 BlkCn BT", 14.25F, FontStyle.Regular, GraphicsUnit.Point, 0);
+            button8.Location = new Point(345, 148);
+            button8.Name = "button8";
+            button8.Size = new Size(60, 54);
+            button8.TabIndex = 18;
+            button8.Text = "CLS";
+            button8.UseVisualStyleBackColor = false;
+            button8.Click += button8_Click;
+            // 
+            // button9
+            // 
+            button9.BackColor = Color.AliceBlue;
+            button9.Font = new Font("Swis721 BlkCn BT", 14.25F, FontStyle.Regular, GraphicsUnit.Point, 0);
+            button9.Location = new Point(345, 217);
+            button9.Name = "button9";
+            button9.Size = new Size(60, 198);
+            button9.TabIndex = 19;
+            button9.Text = "=";
+            button9.UseVisualStyleBackColor = false;
+            button9.Click += button9_Click;
+            // 
+            // button10
+            // 
+            button10.BackColor = Color.GhostWhite;
+            button10.Font = new Font("Swis721 BlkCn BT", 14.25F, FontStyle.Regular, GraphicsUnit.Point, 0);
+            button10.Location = new Point(270, 217);
+            button10.Name = "button10";
+            button10.Size = new Size(60, 54);
+            button10.TabIndex = 20;
+            button10.Text = "6";
+            button10.UseVisualStyleBackColor = false;
+            // 
+            // button11
+            // 
+            button11.BackColor = Color.GhostWhite;
+            button11.Font = new Font("Swis721 BlkCn BT", 14.25F, FontStyle.Regular, GraphicsUnit.Point, 0);
+            button11.Location = new Point(190, 217);
+            button11.Name = "button11";
+            button11.Size = new Size(60, 54);
+            button11.TabIndex = 21;
+            button11.Text = "5";
+            button11.UseVisualStyleBackColor = false;
+            // 
+            // button12
+            // 
+            button12.BackColor = Color.GhostWhite;
+            button12.Font = new Font("Swis721 BlkCn BT", 14.25F, FontStyle.Regular, GraphicsUnit.Point, 0);
+            button12.Location = new Point(111, 217);
+            button12.Name = "button12";
+            button12.Size = new Size(60, 54);
+            button12.TabIndex = 22;
+            button12.Text = "4";
+            button12.UseVisualStyleBackColor = false;
+            // 
+            // button13
+            // 
+            button13.BackColor = Color.AliceBlue;
+            button13.Font = new Font("Swis721 BlkCn BT", 14.25F, FontStyle.Regular, GraphicsUnit.Point, 0);
+            button13.Location = new Point(111, 361);
+            button13.Name = "button13";
+            button13.Size = new Size(219, 54);
+            button13.TabIndex = 23;
+            button13.Text = "0";
+            button13.UseVisualStyleBackColor = false;
+            // 
+            // button14
+            // 
+            button14.BackColor = Color.GhostWhite;
+            button14.Font = new Font("Swis721 BlkCn BT", 14.25F, FontStyle.Regular, GraphicsUnit.Point, 0);
+            button14.Location = new Point(111, 290);
+            button14.Name = "button14";
+            button14.Size = new Size(60, 54);
+            button14.TabIndex = 24;
+            button14.Text = "7";
+            button14.UseVisualStyleBackColor = false;
+            // 
+            // button15
+            // 
+            button15.BackColor = Color.GhostWhite;
+            button15.Font = new Font("Swis721 BlkCn BT", 14.25F, FontStyle.Regular, GraphicsUnit.Point, 0);
+            button15.Location = new Point(190, 290);
+            button15.Name = "button15";
+            button15.Size = new Size(60, 54);
+            button15.TabIndex = 25;
+            button15.Text = "8";
+            button15.UseVisualStyleBackColor = false;
+            // 
+            // button16
+            // 
+            button16.BackColor = Color.GhostWhite;
+            button16.Font = new Font("Swis721 BlkCn BT", 14.25F, FontStyle.Regular, GraphicsUnit.Point, 0);
+            button16.Location = new Point(270, 290);
+            button16.Name = "button16";
+            button16.Size = new Size(60, 54);
+            button16.TabIndex = 26;
+            button16.Text = "9";
+            button16.UseVisualStyleBackColor = false;
+            // 
+            // pictureBox1
+            // 
+            pictureBox1.BackgroundImage = (Image)resources.GetObject("pictureBox1.BackgroundImage");
+            pictureBox1.BackgroundImageLayout = ImageLayout.Zoom;
+            pictureBox1.Location = new Point(0, 63);
+            pictureBox1.Name = "pictureBox1";
+            pictureBox1.Size = new Size(105, 83);
+            pictureBox1.TabIndex = 27;
+            pictureBox1.TabStop = false;
+            // 
+            // pictureBox3
+            // 
+            pictureBox3.BackgroundImage = (Image)resources.GetObject("pictureBox3.BackgroundImage");
+            pictureBox3.BackgroundImageLayout = ImageLayout.Zoom;
+            pictureBox3.Location = new Point(0, 134);
+            pictureBox3.Name = "pictureBox3";
+            pictureBox3.Size = new Size(105, 83);
+            pictureBox3.TabIndex = 28;
+            pictureBox3.TabStop = false;
+            // 
+            // pictureBox4
+            // 
+            pictureBox4.BackgroundImage = (Image)resources.GetObject("pictureBox4.BackgroundImage");
+            pictureBox4.BackgroundImageLayout = ImageLayout.Zoom;
+            pictureBox4.Location = new Point(0, 209);
+            pictureBox4.Name = "pictureBox4";
+            pictureBox4.Size = new Size(105, 83);
+            pictureBox4.TabIndex = 29;
+            pictureBox4.TabStop = false;
+            // 
+            // pictureBox5
+            // 
+            pictureBox5.BackgroundImage = (Image)resources.GetObject("pictureBox5.BackgroundImage");
+            pictureBox5.BackgroundImageLayout = ImageLayout.Zoom;
+            pictureBox5.Location = new Point(0, 281);
+            pictureBox5.Name = "pictureBox5";
+            pictureBox5.Size = new Size(105, 83);
+            pictureBox5.TabIndex = 30;
+            pictureBox5.TabStop = false;
             // 
             // TelaCalculadora
             // 
             AutoScaleDimensions = new SizeF(7F, 15F);
             AutoScaleMode = AutoScaleMode.Font;
-
-            BackColor = Color.Pink;
-
             BackColor = Color.LightPink;
-
             ClientSize = new Size(427, 469);
-            Controls.Add(pictureBox2);
+            Controls.Add(pictureBox5);
+            Controls.Add(pictureBox4);
+            Controls.Add(pictureBox3);
             Controls.Add(pictureBox1);
+            Controls.Add(button16);
+            Controls.Add(button15);
+            Controls.Add(button14);
+            Controls.Add(button13);
+            Controls.Add(button12);
+            Controls.Add(button11);
+            Controls.Add(button10);
+            Controls.Add(button9);
+            Controls.Add(button8);
+            Controls.Add(button7);
+            Controls.Add(button6);
+            Controls.Add(button5);
+            Controls.Add(button4);
+            Controls.Add(button3);
+            Controls.Add(button2);
+            Controls.Add(textBox1);
+            Controls.Add(button1);
+            Controls.Add(pictureBox2);
+            Icon = (Icon)resources.GetObject("$this.Icon");
             Name = "TelaCalculadora";
             StartPosition = FormStartPosition.CenterScreen;
             Text = "TelaCalculadora";
-            ((System.ComponentModel.ISupportInitialize)pictureBox1).EndInit();
             ((System.ComponentModel.ISupportInitialize)pictureBox2).EndInit();
+            ((System.ComponentModel.ISupportInitialize)pictureBox1).EndInit();
+            ((System.ComponentModel.ISupportInitialize)pictureBox3).EndInit();
+            ((System.ComponentModel.ISupportInitialize)pictureBox4).EndInit();
+            ((System.ComponentModel.ISupportInitialize)pictureBox5).EndInit();
             ResumeLayout(false);
+            PerformLayout();
         }
 
         #endregion
-
-        private PictureBox pictureBox1;
         private PictureBox pictureBox2;
+        private Button button1;
+        private TextBox textBox1;
+        private Button button2;
+        private Button button3;
+        private Button button4;
+        private Button button5;
+        private Button button6;
+        private Button button7;
+        private Button button8;
+        private Button button9;
+        private Button button10;
+        private Button button11;
+        private Button button12;
+        private Button button13;
+        private Button button14;
+        private Button button15;
+        private Button button16;
+        private PictureBox pictureBox1;
+        private PictureBox pictureBox3;
+        private PictureBox pictureBox4;
+        private PictureBox pictureBox5;
     }
 }
--- TelaCalculadora.cs
+++ TelaCalculadora.cs
@@ -16,5 +16,20 @@ namespace Calculadora
         {
             InitializeComponent();
         }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button9_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button8_Click(object sender, EventArgs e)
+        {
+
+        }
     }
 }
