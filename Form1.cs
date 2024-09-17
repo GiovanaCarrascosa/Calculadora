f8d64efa7bb2cf002890c23fb0c586085988f74f
GiovanaCarrascosa <giovanaa.carrascosa194@gmail.com>
2024-09-17 11:18:59 -0300

    feito a programação do timer da barra

--- Form1.cs
+++ Form1.cs
@@ -6,5 +6,27 @@ namespace Calculadora
         {
             InitializeComponent();
         }
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            progressBar1.Value = progressBar1.Value + 1;
+
+            if (progressBar1.Value == 100)
+            {
+                timer1.Enabled = false;
+
+                this.Hide();
+
+                TelaCalculadora calculadoratela = new TelaCalculadora();
+                calculadoratela.ShowDialog();
+
+                
+            }
+        }
+
+        private void progressBar1_Click(object sender, EventArgs e)
+        {
+
+        }
     }
 }
